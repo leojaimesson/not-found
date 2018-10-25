7c04c5831dfe2fc051332eec389d0db36052b96d
Leo Jaimesson Sousa da Silva <leo.jaimesson.dev@gmail.com>
2018-10-24 23:13:34 -0300

    Variáveis de ambientes pm2 adicionadas.

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,12 +1,12 @@
 const pkgJson = require('./package.json');
+import env from './src/config/Environment';
 
 module.exports = {
   apps : [{
     name: pkgJson.name,
     script: pkgJson.prodMain,
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G'
+    instances: env.pm2.instances,
+    autorestart: env.pm2.autorestar,
+    max_memory_restart: env.pm2.maxMemoryRestart
   }]
 };
