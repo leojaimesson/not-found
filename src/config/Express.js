416c4582689431dd7d81ca7bdcb20c5ec7653654
Leo Jaimesson Sousa da Silva <leo.jaimesson.dev@gmail.com>
2018-11-24 12:50:23 -0300

    cors

--- src/config/Express.js
+++ src/config/Express.js
@@ -29,7 +29,13 @@ export default () => {
     extended: false,
   }));
 
-  app.use(cors());
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+  });
 
   settingRoutes(app);
 
