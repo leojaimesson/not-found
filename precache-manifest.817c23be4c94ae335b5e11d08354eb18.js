4641757af09da4c3816ec102d05cb77ceb5f6c79
Leo Jaimesson Sousa da Silva <leo.jaimesson.dev@gmail.com>
2018-11-24 12:57:46 -0300

    beta

---
+++ precache-manifest.817c23be4c94ae335b5e11d08354eb18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3018e419b991fc7d63bf",
+    "url": "/static/js/main.3018e419.chunk.js"
+  },
+  {
+    "revision": "743290ad5a7145546378",
+    "url": "/static/js/1.743290ad.chunk.js"
+  },
+  {
+    "revision": "3018e419b991fc7d63bf",
+    "url": "/static/css/main.8cafbbe2.chunk.css"
+  },
+  {
+    "revision": "743290ad5a7145546378",
+    "url": "/static/css/1.a14ee10c.chunk.css"
+  },
+  {
+    "revision": "66253f441d6721b1c3dfed414b75b603",
+    "url": "/index.html"
+  }
+];
